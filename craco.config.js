da4b8423fbebe14e36683f3850baf82ae26dae7c
arr_cole <andrew.rr.cole@gmail.com>
2020-10-17 16:41:02 -0400

    updated styling

--- craco.config.js
+++ craco.config.js
@@ -8,10 +8,25 @@ module.exports = {
         lessLoaderOptions: {
           lessOptions: {
             modifyVars: { 
-              '@primary-color': '#1DA57A',
-              '@link-color' : '#1890ff', 
+              // Primaries
+              '@primary-color': '#722ed1',
+              '@link-color' : '#d3adf7', 
+              '@success-color' : '#73d13d', 
+              '@warning-color' : '#c41d7f', 
+              '@error-color' : '#f5222d', 
               '@heading-color' : 'rgba(0,0,0,0.85)', 
               '@text-color' : 'rgba(0,0,0,0.65)', 
+              '@border-radius-base' : '12px', 
+              '@border-color-base' : '#d5d5d5', 
+              '@font-size-base' : '14px',
+
+              // Dropdown
+
+              '@dropdown-menu-bg': '#eeeeee',
+
+
+              // card
+
             },
             javascriptEnabled: true,
           },
