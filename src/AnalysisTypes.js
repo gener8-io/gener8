3fccdc61f6d556bcba9ab237cf18d2846965e38e
Arif Hanif <ahanif@aeieng.com>
2020-10-17 14:48:14 -0400

    some updates

--- src/AnalysisTypes.js
+++ src/AnalysisTypes.js
@@ -1,21 +1,41 @@
-import React from "react";
-import { Row, Select, Typography } from "antd";
+import React, { useState } from "react";
+import { Card, Button, Row, Col, Select, Typography, Switch } from "antd";
 
 const { Option } = Select;
 const { Title } = Typography;
 
-const AnalysisType = () => (
-  <div>
-    <Row>
-    <Title level={3}>Analysis Type</Title>
-    </Row>
-    <Row>
-      <Select defaultValue="circulation_area">
-        <Option value="circulation_area">Circulation Area</Option>
-        <Option value="bed_size">Bed Size</Option>
-      </Select>
-    </Row>
-  </div>
-);
+const AnalysisType = ({ analysisTypes, setAnalysisTypes }) => {
+  const [currentAnalysis, setCurrentAnalysis] = useState(null);
+  return (
+    <div style={{ padding: 40 }}>
+      <Row>
+        <Title level={3}>Analysis Type</Title>
+      </Row>
+      <Row>
+        <Col>
+          <Select
+            defaultValue="circulation_area"
+            onChange={(value) => setCurrentAnalysis(value)}
+          >
+            <Option value="circulation_area">Circulation Area</Option>
+            <Option value="bed_size">Bed Size</Option>
+          </Select>
+        </Col>
+        <Col>
+          <Button
+            onClick={() => setAnalysisTypes([...analysisTypes, currentAnalysis])}
+          >
+            Add
+          </Button>
+        </Col>
+      </Row>
+      {analysisTypes.map((at) => (
+        <Card>
+          {at.label} <Switch></Switch>
+        </Card>
+      ))}
+    </div>
+  );
+};
 
 export { AnalysisType };
