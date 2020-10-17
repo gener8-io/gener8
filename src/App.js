bc2dc0719baca82075f43e63cd9741e993a7575c
arr_cole <andrew.rr.cole@gmail.com>
2020-10-17 18:07:53 -0400

    Room component added to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+
+import React, { useState } from "react";
+import { Row, Col } from "antd";
 import { AnalysisType } from "./AnalysisTypes";
+import Constraints from "./Constraints";
+import {Room} from './Room';
 import "./App.less";
 
-function App() {
-  return <AnalysisType />;
-}
+const App = () => {
+  const [analysisTypes, setAnalysisTypes] = useState(["Bed Size"]);
+  const [constraints, setConstraints] = useState({ bedSize: "Queen" });
+
+  return (
+    <Row style={{ padding: 40 }}>
+      <Col>
+        <AnalysisType
+          analysisTypes={analysisTypes}
+          setAnalysisTypes={setAnalysisTypes}
+          span={8}
+        />
+      </Col>
+      <Col span={8}>
+        <div>Gallery</div>
+      </Col>
+      <Col span={8}>
+        <Constraints
+          constraints={constraints}
+          setConstraints={setConstraints}
+        />
+      </Col>
+      <Room posx={1} posy={1}/>
+    </Row>
+  );
+};
 
 export default App;
