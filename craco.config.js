011d7032a463269290d93585a3997c29fe050a69
arr_cole <andrew.rr.cole@gmail.com>
2020-10-17 14:18:17 -0400

    Update craco.config.js
    test-cracopush

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,12 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
+            modifyVars: { 
+              '@primary-color': '#1DA57A',
+              '@link-color' : '#1890ff', 
+              '@heading-color' : 'rgba(0,0,0,0.85)', 
+              '@text-color' : 'rgba(0,0,0,0.65)', 
+            },
             javascriptEnabled: true,
           },
         },
